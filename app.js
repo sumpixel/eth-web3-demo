3dd5189514f741e8bbe1332697d12800be8ce4b8
Sum Ling <sumling@isuntv-hk.com>
2017-03-14 18:50:16 +0800

    Fix web3.js submodule

--- app.js
+++ app.js
@@ -1,27 +1,71 @@
-import Promise from 'bluebird';
-import Web3 from './web3'
 import net from 'net';
+import deasync from 'deasync';
+import Web3 from './packages/web3.js'
+
 const web3 = new Web3();
 
-// const websocketPath = 'ws://192.168.100.93:8546';
-const websocketPath = 'ws://localhost:8546';
-const wei = 0.0001;
+const ipcPath = '/Users/sum/Library/Ethereum/testnet/geth.ipc';
+
+const wei = 0.001;
 const from = '0x97e5fe0559f80a83df67578d1f00c6afc9c66bf4';
 const fromPassword = 'sumling';
-const to = '0xb3853931c98cc5925bec4a39fb7c336e7b990869';
-// const to = '0x1D548De4048fd22baAe01b7f614469f6412AD3ED';
+const to = '0x58B35891710be4E0b347204BA2F0269E4730FFf7';
 
-// WebSocket connection
-const connectionProvider = new web3.providers.WebsocketProvider(websocketPath);
+// IPC connection
+const connectionProvider = new web3.providers.IpcProvider(ipcPath, net);
+web3.setProvider(connectionProvider);
+
 let isConnected = false;
-connectionProvider.connection.onopen = () => {
+connectionProvider.on('connect', () => {
   isConnected = true;
   console.log('Connected!');
-};
-require('deasync').loopWhile(() => {
+});
+deasync.loopWhile(() => {
   return !isConnected;
 });
-web3.setProvider(connectionProvider);
+
+// get current block number
+
+let currentBlockNumber;
+web3.eth.getBlockNumber()
+  .then((blockNumber) => {
+    currentBlockNumber = blockNumber;
+  });
+
+deasync.loopWhile(() => {
+  return currentBlockNumber === undefined;
+});
+console.log('Current Block Number:', currentBlockNumber);
+
+// get source account info
+
+let fromBalance;
+web3.eth.getBalance(from)
+  .then((wei) => {
+    fromBalance = web3.utils.fromWei(wei, 'ether');
+  });
+deasync.loopWhile(() => {
+  return fromBalance === undefined;
+});
+console.log('*** from ***');
+console.log('account', from);
+console.log('balance', fromBalance);
+
+// get destination account info
+
+let toBalance;
+web3.eth.getBalance(to)
+  .then((wei) => {
+    toBalance = web3.utils.fromWei(wei, 'ether');
+  });
+deasync.loopWhile(() => {
+  return toBalance === undefined;
+});
+console.log('*** to ***');
+console.log('account', to);
+console.log('balance', toBalance);
+
+// transaction
 
 const amount = web3.utils.toWei(wei, "ether");
 
